3fdf73e0f5578c9468c97a1e2f9dd0cd76aac789
nurisapa <saparbekovanuriza@ymail.com>
2024-04-06 12:51:41 +0000

    added file

--- class2/user.tf
+++ class2/user.tf
@@ -8,5 +8,27 @@ resource "aws_iam_user" "lb" {
   name = each.key
 }
 
+resource "aws_iam_group" "hello" {
+  name = "develops"
+}
+
+# resource "aws_iam_group_membership" "team" {
+#   name = "tf-testing-group-membership"
+
+#   users = [
+#     for i in aws_iam_user.lb : i.name 
+#     ]
+
+#   group = aws_iam_group.hello.name
+# }
+
+#terraform state list - to see what we created 
+# terraform state show aws_iam_group.hello - to see the information 
+#terraform state show 'aws_iam_user.lb["user1"]' - to see the information abput user 
 
 
+# resource "aws_iam_group" "hello" {
+#   name = "develops"
+# }
+
+#terraform taint aws_iam_group.hello- if we want to recreart 
