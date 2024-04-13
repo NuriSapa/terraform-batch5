928a5853d0802a972116c218d516b2dd58f09f60
nurisapa <saparbekovanuriza@ymail.com>
2024-04-13 17:02:39 +0000

    added

---
+++ class6-workspace/main.tf
@@ -0,0 +1,16 @@
+provider aws {
+    region = var.region 
+}
+
+variable region {
+    default = ""
+    type = string
+  
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "kaizen"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#terraform workspace = command 
