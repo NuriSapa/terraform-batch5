a5be456e7a87bf1091cf0078f5fa5beff40e5607
nurisapa <saparbekovanuriza@ymail.com>
2024-04-27 06:57:06 +0000

    added

---
+++ Project/main.tf
@@ -0,0 +1,157 @@
+#VPC named “group-4”, 3 subnets, route table, internet gateway, subnet 
+# associations.
+# Security Group named “group-4”. Open required ports for your application.
+# EC2 instances named “blue-group-4” and “green-group-4” with any desired 
+# Linux flavor for ami image, and make sure ami image is accessible in every 
+# region. You should be able to ssh passwordless to EC2 instance from Bastion 
+# host.
+# Create Load Balancer and target groups.
+# Perform Blue-Green Deployment.
+# Make your code dynamic and create variables and tfvars
+# Statefile should be stored in a remote backend
+# Create module from your code and push to Terraform registry with Readme 
+# file.
+# Create documentation with GitHub link and provide hours for each team 
+
+
+provider aws {
+  region = var.region
+}
+
+resource "aws_key_pair" "grp4-keypair" {
+  key_name   = var.key_name
+  public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Name = var.key_name
+  }
+}
+
+resource "aws_vpc" "group-4" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "grp4-subnet-1" {
+  availability_zone = "${var.region}a"
+  vpc_id     = aws_vpc.group-4.id
+  cidr_block = var.subnet1_cidr
+  map_public_ip_on_launch =var.ip_on_launch
+
+  tags = {
+    Name = var.subnet1_name
+  }
+}
+resource "aws_subnet" "grp4-subnet-2" {
+  availability_zone = "${var.region}b"
+  vpc_id     = aws_vpc.group-4.id
+  cidr_block = var.subnet2_cidr
+   map_public_ip_on_launch =var.ip_on_launch
+
+  tags = {
+    Name = var.subnet2_name
+  }
+}
+
+resource "aws_subnet" "grp4-subnet-3" {
+  availability_zone = "${var.region}c"
+  vpc_id     = aws_vpc.group-4.id
+  cidr_block = var.subnet3_cidr
+   map_public_ip_on_launch =var.ip_on_launch
+
+  tags = {
+    Name = var.subnet3_name
+  }
+}
+
+resource "aws_internet_gateway" "grp4-gw" {
+  vpc_id = aws_vpc.group-4.id
+
+  tags = {
+    Name = var.IGW_name
+  }
+}
+
+resource "aws_route_table" "grp4-rt" {
+  vpc_id = aws_vpc.group-4.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.grp4-gw.id
+  }
+
+  tags = {
+    Name = var.rt_name
+  }
+}
+
+resource "aws_route_table_association" "grp4-rta-1" {
+  subnet_id      = aws_subnet.grp4-subnet-1.id
+  route_table_id = aws_route_table.grp4-rt.id
+}
+
+resource "aws_route_table_association" "grp4-rta-2" {
+  subnet_id      = aws_subnet.grp4-subnet-1.id
+  route_table_id = aws_route_table.grp4-rt.id
+}
+
+resource "aws_route_table_association" "grp4-rta-3" {
+  subnet_id      = aws_subnet.grp4-subnet-1.id
+  route_table_id = aws_route_table.grp4-rt.id
+}
+
+resource "aws_lb" "app" {
+  name               = var.load_balancer_name
+  internal           = false
+  load_balancer_type = var.load_balancer_type
+  subnets            = [
+    aws_subnet.grp4-subnet-1.id,
+    aws_subnet.grp4-subnet-2.id,
+    aws_subnet.grp4-subnet-3.id
+  ]
+  security_groups    = [
+    aws_security_group.group-4.id
+  ]
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = var.lb_listener_port
+  protocol          = var.lb_listener_protocol
+
+  default_action {
+    type             = "forward"
+    #target_group_arn = aws_lb_target_group.blue.arn
+    forward {
+      target_group {
+        arn = aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
+
+      target_group {
+        arn = aws_lb_target_group.green.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      }
+      stickiness {
+        enabled = false
+        duration = 1
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
